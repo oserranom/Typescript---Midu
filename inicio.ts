9d3580ed5230daca0b199c37a1dbc0106c316cb8
Óscar <oscar.serrano91@gmail.com>
2025-06-29 18:04:25 +0200

    inferencia, functions y optional properties

--- inicio.ts
+++ inicio.ts
@@ -1,1 +1,46 @@
-let nombre = 'Oscar';
+//let nombre = 'Oscar';
+
+
+let persona = {
+    name: 'Oscar',
+    age: 33,
+    heigh: 180,
+    weight: 87
+}
+
+//Functions
+//No podemos tipar como (nombre: string, age: number) porque entra en conflito con la sintáxis JS
+//Cuando renombramos variables en la function 
+
+//tipando argumentos y valores de retorno
+function saludar(persona: {nombre: string, age: number}): number {
+
+    const {nombre, age} = persona;
+
+    console.log(`Hola ${nombre}, tienes ${age} años`);
+
+    return age; 
+}
+
+
+
+//Optional properties
+
+type Hero = {
+    readonly id?: number //Opcional y solo lectura 
+    name: string
+    age: number
+    isActive?: boolean
+}
+
+let hero: Hero = {
+    name: 'Thor',
+    age: 1500
+};
+
+function createHero(hero: Hero): Hero{
+    const { name, age } = hero; 
+    return { name, age, isActive: true}
+}
+
+const thor = createHero({ name: 'Thor', age: 1500});
